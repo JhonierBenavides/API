b5ad233f9c6069465bc34fab6aba418d03517bf0
Jhonnier Benavides <63086134+JhonierBenavides@users.noreply.github.com>
2020-05-29 20:28:19 -0500

    CorregirErrores
    -Correccion de errores en la base de datos y agregamos un Procedure.

--- db/db.sql
+++ db/db.sql
@@ -2,18 +2,18 @@ CREATE DATABASE IF NOT EXISTS company;
 
 USE company;
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT(11) NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) DEFAULT NULL,
   salary INT(11) DEFAULT NULL, 
   PRIMARY KEY(id)
 );
 
-DESCRIBE employee;
+DESCRIBE employees;
 
-INSERT INTO employee values 
+INSERT INTO employees values 
   (1, 'Ryan Ray', 20000),
   (2, 'Joe McMillan', 40000),
   (3, 'John Carter', 50000);
 
-SELECT * FROM employee;
+SELECT * FROM employees;
