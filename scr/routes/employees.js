779120329f1b1a952a99e3417a612c933f69b3a4
Diego362 <63076450+Diego362@users.noreply.github.com>
2020-05-29 20:22:04 -0500

    GET agregar
    -Se crea nuevo get para agregar employees

--- scr/routes/employees.js
+++ scr/routes/employees.js
@@ -14,4 +14,16 @@ router.get('/', (req, res) => {
   });  
 });
 
+// GET An Employee
+router.get('/:id', (req, res) => {
+  const { id } = req.params; 
+  mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+    if (!err) {
+      res.json(rows[0]);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
 module.exports = router;
