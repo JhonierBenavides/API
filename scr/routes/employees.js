6a897e2be224ffd4c567c27e357cd29ae8963de7
Jhonnier Benavides <63086134+JhonierBenavides@users.noreply.github.com>
2020-05-29 19:33:10 -0500

    Luis Mora
    -Se creo el GET para el API

--- scr/routes/employees.js
+++ scr/routes/employees.js
@@ -3,5 +3,15 @@ const router = express.Router();
 
 const mysqlConnection  = require('../database.js'); 
 
+// GET all Employees
+router.get('/', (req, res) => {
+  mysqlConnection.query('SELECT * FROM employees', (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+});
 
 module.exports = router;
