57b57ae08961397eda16e172c78a3acd1f536819
JohanAldana191 <66149902+JohanAldana191@users.noreply.github.com>
2020-05-29 21:18:52 -0500

    PROCEDURE AGREGADO
    -Ayuda con los procesos de insert,update,put,delete

---
+++ employeesAddOrEdit.sql
@@ -0,0 +1,25 @@
+USE company;
+
+DELIMITER $$
+USE `company`$$
+
+CREATE PROCEDURE `employeesAddOrEdit`(
+	IN _id INT,
+	IN _name VARCHAR(100),
+	IN _salary INT
+)
+BEGIN
+	IF _id = 0 THEN 
+		INSERT INTO employees(NAME,salary)
+		VALUES (_name, _salary);
+		SET _id= LAST_INSERT_ID();
+	ELSE
+		UPDATE employees
+		SET
+			NAME=_name,
+			salary=_salary
+			WHERE id=id;
+	END IF;
+    
+    SELECT _id AS 'id';
+END
